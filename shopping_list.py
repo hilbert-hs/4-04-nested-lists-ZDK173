bc7f824547127adbe46c0556687dc63f20ed0664
Zachary K. <zkesler2027@gmail.com>
2025-02-20 17:27:48 +0000

    All Pt 1 func and input.

--- shopping_list.py
+++ shopping_list.py
@@ -7,9 +7,28 @@ shopping_cart = [
 
 
 # user inputs
-
-
-
+User_inp = str(input("What would you like to do?"))
+if User_inp = "update":
+  update_list(s_list)
+if User_inp = "view item":
+  print_item(s_list)
+if User_inp = "veiw list":
+  print_list(s_list)
 
 # functions
+def update_list(s_list):
+  row = int(input("Which list would you like to update?"))
+  col = int(input("Which item in list?"))
+  new_item = str(input("What is the item?"))
+  return(s_list[row][col] = new_item)
+
+def print_item(s_list):
+  row = int(input("Which list would you like to update?"))
+  col = int(input("Which item in list?"))
+  return(s_list[row][col])
+
+def print_list(s_list):
+  row = int(input("Which list would you like to update?"))
+  return(s_list[row])
+
 
